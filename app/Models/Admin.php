1a01336d24084dc26218a3b6ed0b738f98bf4b79
theonicolas19@outlook.com <LightBurning1>
2022-03-07 14:25:29 +0100

    FEAT(admins) : Add remember_token

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -15,7 +15,8 @@ class Admin extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password'
+        'password',
+        'remember_token',
     ];
 
     public function setPasswordAttribute($value)
