7ebf58b8656108d96cec1fe25fcdbbd51eb2e855
theonicolas19@outlook.com <LightBurning1>
2022-03-08 15:00:09 +0100

    FEAT(api) : Get api_token

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->update([
+            'api_token' => $user->setApiTokenAttribute()
+        ]);
+
+        return $user;
     }
 }
