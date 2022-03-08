d3f5c866a9471407c70e43feefd8067757b26e45
theonicolas19@outlook.com <LightBurning1>
2022-03-08 15:58:32 +0100

    FEAT(api) : get characters

--- database/seeders/CharactersSeeder.php
+++ database/seeders/CharactersSeeder.php
@@ -27,28 +27,28 @@ class CharactersSeeder extends Seeder
         $characters = new Collection($characters);
         foreach ($characters as $character) {
             if (!$character->npcId == null && !$character->npcId == '') {
-                $month = Carbon::parse(DateTime::createFromFormat('!m', strstr($character->birthday, '/', true)))->locale('fr')->getTranslatedMonthName();
-
+                $month = Carbon::parse(DateTime::createFromFormat('!m', strstr($character->birthday, '/', true)))->locale('en')->getTranslatedMonthName();
                 $characterBDD['en'][$character->npcId]['name'] = $character->name;
-                $characterBDD['en'][$character->npcId]['npcId'] = $character->translations->eUen;
+                $characterBDD['en'][$character->npcId]['npcId'] = $character->npcId;
                 $characterBDD['en'][$character->npcId]['photoImage'] = $character->photoImage;
                 $characterBDD['en'][$character->npcId]['iconImage'] = $character->iconImage;
                 $characterBDD['en'][$character->npcId]['gender_id'] = Gender::getGender('en', substr($character->gender, 0, 1))->id;
-                $characterBDD['en'][$character->npcId]['birthday_month'] = $month;
-                $characterBDD['en'][$character->npcId]['birthday_day'] =  strstr($character->birthday, '/');
+                $characterBDD['en'][$character->npcId]['birthday_month'] = ucfirst($month);
+                $characterBDD['en'][$character->npcId]['birthday_day'] =  substr(strstr($character->birthday, '/'), 1);
                 $characterBDD['en'][$character->npcId]['lang_id'] = LanguageData::getEn()->id;
 
-                $characterBDD['fr'][$character->npcId]['name'] = $character->name;
-                $characterBDD['fr'][$character->npcId]['npcId'] = $character->translations->eUen;
+                $month = Carbon::parse(DateTime::createFromFormat('!m', strstr($character->birthday, '/', true)))->locale('fr')->getTranslatedMonthName();
+
+                $characterBDD['fr'][$character->npcId]['name'] = $character->translations->eUfr;
+                $characterBDD['fr'][$character->npcId]['npcId'] = $character->npcId;
                 $characterBDD['fr'][$character->npcId]['photoImage'] = $character->photoImage;
                 $characterBDD['fr'][$character->npcId]['iconImage'] = $character->iconImage;
                 $characterBDD['fr'][$character->npcId]['gender_id'] = Gender::getGender('fr', substr($character->gender, 0, 1))->id;
-                $characterBDD['fr'][$character->npcId]['birthday_month'] = $character->birthday_month;
-                $characterBDD['fr'][$character->npcId]['birthday_day'] = $character->birthday_day !== '' ? $character->birthday_day : null;
+                $characterBDD['fr'][$character->npcId]['birthday_month'] = ucfirst($month);
+                $characterBDD['fr'][$character->npcId]['birthday_day'] = substr(strstr($character->birthday, '/'), 1);
                 $characterBDD['fr'][$character->npcId]['lang_id'] = LanguageData::getFr()->id;
             }
         }
-
 
         foreach ($characterBDD['en'] as $character) {
             Character::create($character);
