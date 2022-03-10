d1aba12bfe3f56bd2f637f35d25800dd32c04c81
theonicolas19@outlook.com <LightBurning1>
2022-03-10 14:20:44 +0100

    FEAT(api) : user connexion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,7 +18,7 @@ class Controller extends BaseController
                 'id' => Auth::guard('user')->user()->id,
                 'api_token' => Auth::guard('user')->user()->api_token,
             ];
-            dd('test');
+            dd(Auth::guard('user')->user());
             return json_encode($user);
         }
 
