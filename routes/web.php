1633127ffc4c722e594114866e58844002956df1
theonicolas19@outlook.com <LightBurning1>
2022-03-10 14:15:16 +0100

    FEAT(api) : user connexion

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 Route::group([
     'prefix' => 'api/',
