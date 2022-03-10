5697e7f317d9988961b446e9acad33d07d5341ef
theonicolas19@outlook.com <LightBurning1>
2022-03-10 11:32:29 +0100

    FEAT(api) : user cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
+        $response->header('Access-Control-Allow-Methods', 'HEAD, POST, PUT, DELETE');
         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         $response->header('Access-Control-Allow-Origin', '*');
 
