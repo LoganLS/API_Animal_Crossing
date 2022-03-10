66abbd65e3131c0b481aec64ba71f3bf23f032a5
theonicolas19@outlook.com <LightBurning1>
2022-03-10 14:17:09 +0100

    FEAT(api) : user connexion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,6 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     function getApiToken() {
-        dd(auth('user')->check());
         if (Auth::guard('user')->user() !== null) {
             $user = [
                 'id' => Auth::guard('user')->user()->id,
