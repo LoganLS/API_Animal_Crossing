4068ac58f9d8dafff7112182e24a8d98484c39e1
theonicolas19@outlook.com <LightBurning1>
2022-03-10 11:31:31 +0100

    FEAT(api) : user cors

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
 
 Route::group([
     'middleware' => ['cors'],
-], function ($router) {
+], function () {
     Route::get('/token', function (Request $request) {
         return csrf_token();
     });
