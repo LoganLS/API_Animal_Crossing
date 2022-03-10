60b0e029eb4ad12bd889aadd0ff7e8c8358c60e1
theonicolas19@outlook.com <LightBurning1>
2022-03-10 15:38:26 +0100

    FEAT(api) : user connexion

--- config/auth.php
+++ config/auth.php
@@ -41,7 +41,7 @@ return [
             'provider' => 'users',
         ],
         'api' => [
-            'driver' => 'session',
+            'driver' => 'token',
             'provider' => 'users',
         ],
         'user' => [
