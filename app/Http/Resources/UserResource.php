642d80543f3d7f2dab50464501deddbcdcd4fe82
theonicolas19@outlook.com <LightBurning1>
2022-03-10 17:54:51 +0100

    FEAT(api) : user connexion

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
             'description' => $this->description,
