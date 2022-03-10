6097b7ac5001e5ded5c2271a34f944c47bc194d9
theonicolas19@outlook.com <LightBurning1>
2022-03-10 11:10:42 +0100

    FEAT(api) : user cors

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group([
     });
     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
     Route::post('/login', 'Auth\LoginController@login');
+    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 });
 
 Route::get('/', function () {
