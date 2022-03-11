b18ca5154f2c8a587fb9d2fd69f12bcb1c5bc4e7
mathieu-neyret <neyret.mathieu69@gmail.com>
2022-03-11 09:54:01 +0100

    FEAT(api) : publications seeder

--- database/seeders/PublicationsSeeder.php
+++ database/seeders/PublicationsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Publications;
 use Illuminate\Database\Seeder;
 
 class PublicationsSeeder extends Seeder
@@ -13,6 +14,51 @@ class PublicationsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Publications::query()->delete();
+
+        $set = new Publications();
+        $set['id'] = 1;
+        $set['title'] = 'First';
+        $set['content'] = "C'est la toute première publication youpppiiii";
+        $set['category_id'] = 1;
+        $set['user_id'] = 1;
+
+        $set->save();
+
+        $set = new Publications();
+        $set['id'] = 2;
+        $set['title'] = 'Deuxieme';
+        $set['content'] = "J'adore les villageois et j'adore ce site";
+        $set['category_id'] = 2;
+        $set['user_id'] = 1;
+
+        $set->save();
+
+        $set = new Publications();
+        $set['id'] = 3;
+        $set['title'] = 'Troisieme';
+        $set['content'] = "La page profile est joli !";
+        $set['category_id'] = 3;
+        $set['user_id'] = 1;
+
+        $set->save();
+
+        $set = new Publications();
+        $set['id'] = 4;
+        $set['title'] = 'Quatrieme';
+        $set['content'] = "C'est pété !";
+        $set['category_id'] = 1;
+        $set['user_id'] = 1;
+
+        $set->save();
+
+        $set = new Publications();
+        $set['id'] = 5;
+        $set['title'] = 'Cinquieme';
+        $set['content'] = "Juste pour passer un coucou";
+        $set['category_id'] = 2;
+        $set['user_id'] = 1;
+
+        $set->save();
     }
 }
