bd4f2dca971e9d2d14276701b6b81bce83b879fc
theonicolas19@outlook.com <LightBurning1>
2022-03-11 12:42:19 +0100

    FEAT(api) : Controller API

--- app/Http/Controllers/Api/InsectController.php
+++ app/Http/Controllers/Api/InsectController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Insect;
+use App\Models\LanguageData;
+use App\Models\Villager;
 use Illuminate\Http\Request;
 
 class InsectController extends Controller
@@ -14,7 +17,12 @@ class InsectController extends Controller
      */
     public function index()
     {
+        $insects = Insect::query()
+            ->select('bugs.*', 'languages_data.name AS LangDataName')
+            ->leftJoin('languages_data', 'languages_data.id', '=', 'bugs.lang_id')
+            ->where('languages_data.name', LanguageData::getEn()->name);
 
+        return response()->json($insects->get());
     }
 
     /**
@@ -25,6 +33,8 @@ class InsectController extends Controller
      */
     public function show($id)
     {
-        //
+        $insect = Insect::where('id', $id)->get();
+
+        return response()->json($insect);
     }
 }
