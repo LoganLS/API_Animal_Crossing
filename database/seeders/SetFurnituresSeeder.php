cf562b296d1766bd66be63eaa2c9c7eea0ec8b09
LoganLS <logan_56@hotmail.fr>
2022-03-11 09:34:18 +0100

    feature : controller, seeder setFurniture

--- database/seeders/SetFurnituresSeeder.php
+++ database/seeders/SetFurnituresSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SetFurniture;
 use Illuminate\Database\Seeder;
 
 class SetFurnituresSeeder extends Seeder
@@ -13,6 +14,54 @@ class SetFurnituresSeeder extends Seeder
      */
     public function run()
     {
-        //
+        SetFurniture::query()->delete();
+
+        $set = new SetFurniture();
+        $set['id'] = 1;
+        $set['label'] = 'Série Moderne';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 2;
+        $set['label'] = 'Série bleu';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 3;
+        $set['label'] = 'Série rustique';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 4;
+        $set['label'] = 'Série ranch';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 5;
+        $set['label'] = 'Série royal';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 6;
+        $set['label'] = 'Série enfant';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 7;
+        $set['label'] = 'Série coeur';
+
+        $set->save();
+
+        $set = new SetFurniture();
+        $set['id'] = 8;
+        $set['label'] = 'Série chalet';
+
+        $set->save();
     }
 }
