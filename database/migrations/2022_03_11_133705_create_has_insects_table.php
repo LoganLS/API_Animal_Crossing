0817c30db23cd5b77d131bfc2ae0a46ae05fe6b5
theonicolas19@outlook.com <LightBurning1>
2022-03-11 14:56:19 +0100

    FEAT(api) : 200 commits

---
+++ database/migrations/2022_03_11_133705_create_has_insects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHasInsectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('has_insects', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('insect_id')->constrained('bugs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('has_insects');
+    }
+}
