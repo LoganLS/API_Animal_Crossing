36af6a2fc796a0835931d7ffe478b367a3864c7a
theonicolas19@outlook.com <LightBurning1>
2022-03-11 15:00:55 +0100

    FEAT(api) : 200 commits

--- app/Models/HasInsect.php
+++ app/Models/HasInsect.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class HasInsect extends Model
 {
     use HasFactory;
+
+    protected $table = 'has_insects';
+
+    protected $fillable = [
+        'user_id',
+        'insect_id',
+    ];
 }
