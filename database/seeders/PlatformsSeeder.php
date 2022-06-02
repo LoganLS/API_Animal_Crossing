7e765dc27cc3bf0c75b0041dbf06be618826671a
LoganLS <logan_56@hotmail.fr>
2022-06-02 15:38:44 +0200

    feat(seeder): platforms

--- database/seeders/PlatformsSeeder.php
+++ database/seeders/PlatformsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Platforms;
 use Illuminate\Database\Seeder;
 
 class PlatformsSeeder extends Seeder
@@ -13,6 +14,36 @@ class PlatformsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Platforms::query()->delete();
+
+        $gamecube = new Platforms();
+        $gamecube['id'] = 1;
+        $gamecube['label'] = 'Gamecube';
+        $gamecube['release_date'] = new \DateTime('2004-09-24');
+        $gamecube->save();
+
+        $ds = new Platforms();
+        $ds['id'] = 2;
+        $ds['label'] = 'DS';
+        $ds['release_date'] = new \DateTime('2006-03-31');
+        $ds->save();
+
+        $wii = new Platforms();
+        $wii['id'] = 3;
+        $wii['label'] = 'Wii';
+        $wii['release_date'] = new \DateTime('2008-12-05');
+        $wii->save();
+
+        $nintendo3ds = new Platforms();
+        $nintendo3ds['id'] = 4;
+        $nintendo3ds['label'] = '3DS';
+        $nintendo3ds['release_date'] = new \DateTime('2016-11-25');
+        $nintendo3ds->save();
+
+        $switch = new Platforms();
+        $switch['id'] = 5;
+        $switch['label'] = 'Switch';
+        $switch['release_date'] = new \DateTime('2020-03-20');
+        $switch->save();
     }
 }
